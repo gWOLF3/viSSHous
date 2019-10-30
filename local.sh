65a2dde5ce480ea405624749b8ed13a899ccb524
gWOLF3 <wolfe.t.glenn@gmail.com>
2019-10-30 12:25:29 -0400

    Manual remote command no longer needed. Cleaner.
    - seperated the viSSHous config to seperate file, which gets cleanly
    sourced by .ssh/config, uses 'Include'.
    - cut work in half by eliminating the need to manually run remote
    command with the sshd's 'RemoteCommand' option.
    - added in date to help user know when file was last updated.
    - TODO: enable this script to be run automatically as system daemon, to
    check whenever changes to local .vimrc are made, to update our viSSHous
    config.

--- local.sh
+++ local.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
+if ! grep -qR "Include ~/.ssh/viSSHous" ~/.ssh/config
+  then echo 'Include ~/.ssh/viSSHous' >> ~/.ssh/config
+fi
+  
 
-echo SetEnv LC_MONETARY=$(cat ~/.vimrc | base64) >> ~/.ssh/config
+echo "# last updated: $(date)" > ~/.ssh/viSSHous
+echo "SetEnv LC_MONETARY=$(cat ~/.vimrc | base64)" >> ~/.ssh/viSSHous
+echo 'RemoteCommand echo $LC_MONETARY > ~/.vim/visshnu; base64 --decode ~/.vim/visshnu > ~/.vimrc; bash' >> ~/.ssh/viSSHous
+echo "RequestTTY yes" >> ~/.ssh/viSSHous
--- remote.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-  
-
-echo $LC_MONETARY > ~/.vim/visshnu
-base64 --decode ~/.vim/visshnu > ~/.vimrc
