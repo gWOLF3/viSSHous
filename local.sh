88d6092df0d70591e723eb758ea49110d2a8825f
gWOLF3 <wolfe.t.glenn@gmail.com>
2019-10-30 13:32:38 -0400

    workaround for lack of OpenSSH 7.3 support:
    - prettier solution utilized a simple include statement in the standard
    ~/.ssh/config file, however, this caused problems for servers which
    relied on older version of OpenSSH to authenticate (ie Github, which
    seems to use Vagrant SSH, 7.2). Until 7.3 is widely supported, will use
    this aliasing as workaround for 'Include' statement.

--- local.sh
+++ local.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
+<<OpenSSH-7.3
 if ! grep -qR "Include ~/.ssh/viSSHous" ~/.ssh/config
   then echo 'Include ~/.ssh/viSSHous' >> ~/.ssh/config
 fi
+OpenSSH-7.3
   
+# workaround until OpenSSH 7.3 is widely adopted
+if ! grep -qR 'alias ssh="ssh -F ~/.ssh/viSSHous"' ~/.bashrc
+  then echo 'alias ssh="ssh -F ~/.ssh/viSSHous"' >> ~/.bashrc && source ~/.bashrc
+fi 
 
 echo "# last updated: $(date)" > ~/.ssh/viSSHous
+echo 'Include ~/.ssh/config' >> ~/.ssh/viSSHous
 echo "SetEnv LC_MONETARY=$(cat ~/.vimrc | base64)" >> ~/.ssh/viSSHous
 echo 'RemoteCommand echo $LC_MONETARY > ~/.vim/visshnu; base64 --decode ~/.vim/visshnu > ~/.vimrc; bash' >> ~/.ssh/viSSHous
 echo "RequestTTY yes" >> ~/.ssh/viSSHous
